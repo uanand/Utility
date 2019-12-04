acb5c8433d17036b72fd55af73cb2f2de9746e36
Utkarsh Anand <utkarsh.aanand@gmail.com>
2019-12-04 15:09:32 +0800

    Add parameter for fixing the length of movie.

--- imageSequenceTomovie.py
+++ imageSequenceTomovie.py
@@ -3,9 +3,9 @@ import imageio
 import pandas
 from tqdm import tqdm
 
-df = pandas.read_excel('inputs.xlsx',sheet_name='imageSequenceTomovie',names=['inputDir','firstFrame','lastFrame','inputFileExt','outputFile','outputFileExt','quality','allCompressionFlag'])
+df = pandas.read_excel('inputs.xlsx',sheet_name='imageSequenceTomovie',names=['inputDir','firstFrame','lastFrame','inputFileExt','outputFile','outputFileExt','quality','allCompressionFlag','duration'])
 
-for inputDir,firstFrame,lastFrame,inputFileExt,outputFile,outputFileExt,quality,allCompressionFlag in df.values:
+for inputDir,firstFrame,lastFrame,inputFileExt,outputFile,outputFileExt,quality,allCompressionFlag,duration in df.values:
     frameList = range(firstFrame,lastFrame+1)
     try:
         if (numpy.isnan(outputFile)):
@@ -16,13 +16,15 @@ for inputDir,firstFrame,lastFrame,inputFileExt,outputFile,outputFileExt,quality,
         quality = 5
     if (numpy.isnan(allCompressionFlag)):
         allCompressionFlag = 0
-    fps = int(numpy.ceil(len(frameList)/20.))
+    if (numpy.isnan(duration)):
+        duration = 20
+    fps = int(numpy.ceil(len(frameList)/float(duration)))
     
-    if (allCompressionFlag==1):
+    if (int(allCompressionFlag)==1):
         qualityList = range(1,11)
         for quality in qualityList:
             print ('Saving movie for %s with quality %d' %(inputDir,quality))
-            output = outputFile.replace(outputFileExt,'_compress_'+str(quality).zfill(2)+outputFileExt)
+            output = outputFile.replace(outputFileExt,'_quality_'+str(quality).zfill(2)+outputFileExt)
             w = imageio.get_writer(output,\
                                     format='FFMPEG',\
                                     fps=fps,\
@@ -36,7 +38,7 @@ for inputDir,firstFrame,lastFrame,inputFileExt,outputFile,outputFileExt,quality,
             w.close()
     else:
         print ('Saving movie for %s with quality %d' %(inputDir,quality))
-        output = outputFile.replace(outputFileExt,'_compress_'+str(quality).zfill(2)+outputFileExt)
+        output = outputFile.replace(outputFileExt,'_quality_'+str(int(quality)).zfill(2)+outputFileExt)
         w = imageio.get_writer(output,\
                                 format='FFMPEG',\
                                 fps=fps,\
@@ -48,4 +50,3 @@ for inputDir,firstFrame,lastFrame,inputFileExt,outputFile,outputFileExt,quality,
             gImg = imageio.imread(inputDir+'/'+str(frame).zfill(6)+inputFileExt)
             w.append_data(gImg)
         w.close()
-    
