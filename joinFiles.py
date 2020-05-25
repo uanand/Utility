af0ea72d70b4f38ae3c5732623a446023ccdc4d2
Utkarsh Anand <utkarsh.aanand@gmail.com>
2020-05-25 17:18:31 +0800

    Read 1 GB at a time.

--- joinFiles.py
+++ joinFiles.py
@@ -3,32 +3,12 @@ import os
 from tqdm import tqdm
 import utils
 
-# def findSplitFiles(inputFile):
-    # if (platform.system()=='Linux'):
-        # tempList = inputFile.split('/')
-        # path = ''
-        # for temp in tempList[:-1]:
-            # path = path+temp+'/'
-    # elif (platform.system()=='Windows'):
-        # tempList = inputFile.split('\\')
-        # path = ''
-        # for temp in tempList[:-1]:
-            # path = path+temp+'\\'
-    # fileName = inputFile.split('_split_0001')[0]+'_split_'
-    # dirFileList = listdir(path)
-    # splitFileList = []
-    # for dirFile in dirFileList:
-        # if (fileName in path+dirFile):
-            # splitFileList.append(path+dirFile)
-    # splitFileList = list(numpy.sort(splitFileList,kind='mergesort'))
-    # return splitFileList
-    
 ############################################################
-# READ THE INPUT EXCEL FILE AND SPLIT THE INPUT FILES INTO BLOCKS OF 128 GB
+# READ THE INPUT EXCEL FILE AND COMBINE THE SPLIT FILES
 ############################################################
 df = pandas.read_excel('inputs.xlsx',sheet_name='joinFiles',names=['inputFile','deleteFlag'])
 
-chunkSizeMB = 64 # in MB
+chunkSizeMB = 1024 # in MB
 chunkSize = chunkSizeMB*1024*1024
 
 for inputFile,deleteFlag in df.values:
