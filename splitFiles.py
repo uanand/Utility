d4d39302834708fb44df6230a2640b7674d0fec2
Utkarsh Anand <utkarsh.aanand@gmail.com>
2020-05-26 00:35:44 +0800

    Add save option to another directory.

--- splitFiles.py
+++ splitFiles.py
@@ -1,25 +1,38 @@
 import pandas
 import os
+import numpy
+import platform
 
 ############################################################
 # READ THE INPUT EXCEL FILE AND SPLIT THE INPUT FILES INTO BLOCKS OF 128 GB
 ############################################################
-df = pandas.read_excel('inputs.xlsx',sheet_name='splitFiles',names=['inputFile','deleteFlag'])
+df = pandas.read_excel('inputs.xlsx',sheet_name='splitFiles',names=['inputFile','outputDir','deleteFlag'])
 
 chunkSizeMB = 1024 # in MB
-fileSizeGB = 128 # in GB
+fileSizeGB = 1 # in GB
 zfillVal = 4
 
 chunkSize = chunkSizeMB*1024*1024
 fileSize = fileSizeGB*1024*1024*1024
 numChunks = int(fileSize/chunkSize)
-for inputFile,deleteFlag in df.values:
+for inputFile,outputDir,deleteFlag in df.values:
     inputFileSize = os.path.getsize(inputFile)
     numFiles = int(inputFileSize/fileSize)+1
     print ('Splitting the file %s into %d parts' %(inputFile,numFiles))
+    
+    if (platform.system()=='Linux'):
+        fileName = inputFile.split('/')[-1]
+    elif (platform.system()=='Windows'):
+        fileName = inputFile.split('\\')[-1]
+    try:
+        if (numpy.isnan(outputDir)):
+            outputFile = inputFile
+    except:
+        outputFile = outputDir+'/'+fileName
+        
     chunkNum,splitNum = 0,1
     readFile = open(inputFile,'rb')
-    writeFile = open(inputFile+'_split_'+str(splitNum).zfill(zfillVal),'wb')
+    writeFile = open(outputFile+'_split_'+str(splitNum).zfill(zfillVal),'wb')
     
     while (1):
         chunk = readFile.read(chunkSize)
@@ -32,7 +45,7 @@ for inputFile,deleteFlag in df.values:
             writeFile.close()
             print ('Split number %d saved' %(splitNum))
             splitNum = splitNum+1
-            writeFile = open(inputFile+'_split_'+str(splitNum).zfill(zfillVal),'wb')
+            writeFile = open(outputFile+'_split_'+str(splitNum).zfill(zfillVal),'wb')
             writeFile.write(chunk)
             chunkNum = 1
     print ('Split number %d saved' %(splitNum))
